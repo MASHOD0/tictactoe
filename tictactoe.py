30274e901d982cb29601f0afd4d974b803486017
MASHOD0 <mashhood2002@gmail.com>
2021-05-06 01:26:00 +0530

    added possible_moves() in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -44,3 +44,29 @@ def player(board):
     if x_counter == o_counter:
         return X 
     return O
+
+    def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if board == initial_state():
+        return X
+
+    x_counter = 0
+    o_counter = 0
+    for row in board:
+        x_counter += row.count(X)
+        o_counter += row.count(O)
+
+    if x_counter == o_counter:
+        return X 
+    return O
