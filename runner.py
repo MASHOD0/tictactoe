bd833a8fcacc8122e6c116b005816b3307e8380e
MASHOD0 <mashhood2002@gmail.com>
2021-05-06 01:47:18 +0530

    colors & screen init in runner.py

--- runner.py
+++ runner.py
@@ -6,3 +6,13 @@ import tictactoe as ttt
 
 pygame.init()
 size = width, height = 600, 400
+
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode(size)
+
+mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
+largeFont = pygame.font.Font("OpenSans-Regular.ttf", 40)
+moveFont = pygame.font.Font("OpenSans-Regular.ttf", 60)
