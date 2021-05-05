8273fcf162a3dda523741af14b95e000a044e7c9
MASHOD0 <mashhood2002@gmail.com>
2021-05-06 01:24:26 +0530

    added inital_state() in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -11,3 +11,10 @@ X = "X"
 O = "O"
 EMPTY = None
 
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
