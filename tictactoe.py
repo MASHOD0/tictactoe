791b3946f3c6c2666d455bdd1c020d155976f000
MASHOD0 <mashhood2002@gmail.com>
2021-05-06 01:26:53 +0530

    added actions() in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -54,19 +54,15 @@ def player(board):
             [EMPTY, EMPTY, EMPTY]]
 
 
-def player(board):
-    """
-    Returns player who has the next turn on a board.
-    """
-    if board == initial_state():
-        return X
 
-    x_counter = 0
-    o_counter = 0
-    for row in board:
-        x_counter += row.count(X)
-        o_counter += row.count(O)
-
-    if x_counter == o_counter:
-        return X 
-    return O
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_moves = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_moves.add((i, j))
+                
+    return possible_moves
