c106878ff62b2bd3206f3734402fc27c86189e73
MASHOD0 <mashhood2002@gmail.com>
2021-05-06 01:25:11 +0530

    added player() in tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -18,3 +18,29 @@ def initial_state():
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    if board == initial_state():
+        return X
+
+    x_counter = 0
+    o_counter = 0
+    for row in board:
+        x_counter += row.count(X)
+        o_counter += row.count(O)
+
+    if x_counter == o_counter:
+        return X 
+    return O
